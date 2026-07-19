6cc363e30d5470701eb4511d49e7b77c303bdc10
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-19 20:44:26 +0530

    install

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ USERID=$(id -u)
 
 #Check root access or not 
 if [ $USERID -ne 0 ]; then
-echo "Please run this script with root access"
-exit 1
+    echo "Please run this script with root access"
+    exit 1
 fi 
 
 echo "Installing MySQl"
