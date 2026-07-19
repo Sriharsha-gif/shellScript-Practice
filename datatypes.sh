abb6ceea048795f2fb00796da5c05fcdf5bb0c24
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-19 18:11:31 +0530

    datatypes

--- datatypes.sh
+++ datatypes.sh
@@ -10,6 +10,6 @@ NAMES=("Varanasi" "BOB" "MAHESH")
 #ndex always start from 0
 
 echo "Names are : ${NAMES[@]}"
-echo "First Name : ${NAMEs[0]}"
+echo "First Name : ${NAMES[0]}"
 echo "Second Name : ${NAMES[1]}"
 ecoho "Third Name : ${NAMES[2]}"
