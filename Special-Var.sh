1dd690c0f3da4d0095d246b86c2258142b400c05
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-19 17:17:03 +0530

    Special-Var

--- Special-Var.sh
+++ Special-Var.sh
@@ -3,3 +3,6 @@
 ##--------------------Special Variables ------------------------##
 
 echo "All variables passed to Script : $@"
+echo "Number of Variables Passed : $#"
+echo "Frist variable : $1"
+echo "Script Name : $0"
