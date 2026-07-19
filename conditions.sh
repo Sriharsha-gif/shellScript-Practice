e797eefbff1c817b057f37e475fdea592494f3be
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-19 20:37:58 +0530

    install

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given Numeber $NUMBER is Greater to 20"
-elif [ $NUMBER -eq 20 ]; then
+elif [ $NUMBER -eq 20 ]; then 
     echo " Given Number $NUMBER is equal Than 20"
 else 
      echo " Given Number $NUMBER is Less Than 20"
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+USER=$(id -u)
+
+#Check root access or not 
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root access"
+exit 1
+fi 
+
+echo " Installing MySQl"
+dnf Install mysql -y
+
+if [$? -ne 0 ];then 
+echo "Istalling MYSQL is ..........Failed"
+exit 1
+else
+echo " Installing MYSQL is .........SUCCESS"
+fi
