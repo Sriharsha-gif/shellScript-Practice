e1b68d74077c67d295a381df7786e5618c861afb
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-28 21:59:43 +0530

    Install

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi 
+#1st Arg - What are you trying to install
+#2nd Arg - exit ocde
+VALIDTAE (){
+    if [ $? -ne 0 ]; then
+        echo "Installing MYSQL is ..........Failed"
+        exit 1
+else
+    echo " Installing MYSQL is .........SUCCESS"
+fi
+
+}
 
 
 #
@@ -18,11 +29,14 @@ echo "MYSQL is Already Install....SKIPPED"
 else 
 echo "Installing MySQl"
 dnf install mysql -y
-
-if [ $? -ne 0 ]; then
-echo "Installing MYSQL is ..........Failed"
-exit 1
-else
-echo " Installing MYSQL is .........SUCCESS"
+VALIDATE MYSQL $?
 fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "nginx ia already installed....Skipping"
+else
+    echo "installing ngonx"
+    dnf install ngnix -y
+VALIDATE MySQl $?
 fi
