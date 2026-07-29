d56210ea779889b18a16c074e0d57c25642752ff
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-29 22:52:04 +0530

    api doc added

--- loops2.sh
+++ loops2.sh
@@ -23,6 +23,12 @@ VALIDATE(){
   for package in $@
   do 
    echo " Installing $package"
+   dnf list installed $package
+   if [ $? -ne 0]; then
+    dnf install $package -y &..$LOGS_FILE
+    VALIDATE $package $?
+  else
+  echo "$package already installed..........Skipping"
   done
 
 
