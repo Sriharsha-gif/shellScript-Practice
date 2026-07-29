ec52740a9e1baad112f400d4744c1a8a2040e1a7
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-07-29 22:55:24 +0530

    api doc added

--- loops2.sh
+++ loops2.sh
@@ -24,7 +24,7 @@ VALIDATE(){
   do 
    echo " Installing $package"
    dnf list installed $package
-   if [ $? -ne 0]; then
+   if [ $? -ne 0 ]; then
     dnf install $package -y &..$LOGS_FILE
     VALIDATE $package $?
   else
