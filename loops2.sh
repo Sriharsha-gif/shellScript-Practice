43abf9e59150066b0fff2523aebfe243959e2079
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-10 21:05:24 +0530

    api doc added

--- loops2.sh
+++ loops2.sh
@@ -28,7 +28,7 @@ VALIDATE(){
  }
   for package in $@
   do 
-   echo "$TIMESTAMP [INFO] Installing $package"
+   echo -e "$TIMESTAMP [INFO] Installing $package"
    dnf list installed $package &>> @LOGS_FILE
    if [ $? -ne 0 ]; then
     dnf install $package -y &>> $LOGS_FILE
