e9bdc0ef1696f80e30a37108fdcc414e73906c7e
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-10 21:13:55 +0530

    api doc added

---
+++ set.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
+#Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+#1st arg >> What are you trying to instal
+#2nd >> exit code
+
+  for package in $@
+  do 
+   echo -e "$TIMESTAMP [INFO] Installing $package"
+   dnf list installed $package &>> @LOGS_FILE
+   if [ $? -ne 0 ]; then
+    dnf install $package -y &>> $LOGS_FILE
+  
+  else
+    echo -e "$TIMESTAMP [INFO] $package already installed.......... $Y Skipping $N"
+  fi
+  done
+
+
