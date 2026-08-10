2ee0e09b341d38fdfd7015f5ca78f89f2b2fc7b4
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-10 21:04:07 +0530

    api doc added

--- loops2.sh
+++ loops2.sh
@@ -3,7 +3,12 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S") 
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
 
 #Check root access or not
 if [ $USERID -ne 0 ]; then
