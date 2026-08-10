6a88b2601e7d4549e91daa4ed4322fafdd1341a4
Sriharsha-gif <gorantlasriharsha0@gmail.com>
2026-08-10 20:48:05 +0530

    api doc added

--- loops2.sh
+++ loops2.sh
@@ -2,7 +2,8 @@
 
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
-LOGS_FILE="$LOGS_DIR/$0.log" 
+LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S") 
 
 #Check root access or not
 if [ $USERID -ne 0 ]; then
@@ -13,10 +14,10 @@ fi
 #2nd >> exit code
 VALIDATE(){
     if [ $2 -ne 0 ];then
-     echo "Installing $1 is.........FAILED"
+     echo "$TIMESTAM [ERROR] Installing $1 is.........FAILED" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "Install $1 is ...sucess"
+        echo "TIMESTAMP [INFO]Install $1 is ...sucess" | tee -a $LOGS_FILE
     fi
 
  }
